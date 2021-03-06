534987344d55a5d1832407cb0579461078ecd2fe
Dean Cochran <dean.cochran@centre.edu>
2021-03-06 16:00:34 -0600

    good stopping point

--- Jquery/navbar.js
+++ Jquery/navbar.js
@@ -4,7 +4,7 @@ var scrollposition = window.pageYOffset;
               if (scrollposition > currentScrollPos) {
                   document.getElementById("navbar").style.top = "0";
                 } else {
-                  document.getElementById("navbar").style.top = "-100px";
+                  document.getElementById("navbar").style.top = "-150px";
                 }
               scrollposition = currentScrollPos;
             }
