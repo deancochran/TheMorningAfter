ce822929895fddf763b9c5d4732645568ba8bc09
Dean Cochran <dean.cochran@centre.edu>
2021-03-06 00:48:34 -0600

    css update

--- Jquery/navbar.js
+++ Jquery/navbar.js
@@ -1,10 +1,10 @@
 var scrollposition = window.pageYOffset;
             window.onscroll = function() {
             var currentScrollPos = window.pageYOffset;
-            if (scrollposition > currentScrollPos) {
-                document.getElementById("navbar").style.top = "0";
-              } else {
-                document.getElementById("navbar").style.top = "-120px";
-              }
+              if (scrollposition > currentScrollPos) {
+                  document.getElementById("navbar").style.top = "0";
+                } else {
+                  document.getElementById("navbar").style.top = "-120px";
+                }
               scrollposition = currentScrollPos;
             }
